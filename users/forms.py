3b65e2bf2e1ec9430bfdea09aab70b820d4939a5
Jeff Hu <jhh3000@gmail.com>
2016-02-04 21:16:07 -0500

    minor cleanup

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,3 @@ class UserRegistrationForm(forms.Form):
     fl_name = forms.CharField(label='First and Last Name', max_length=50)
     email = forms.CharField(label='Email', max_length=50)
     password = forms.CharField(label='Password', max_length=50)
-
-class UserLoginForm(forms.Form):
-    email = forms.CharField(label='Email', max_length=50)
-    password = forms.CharField(label='Password', max_length=50)
